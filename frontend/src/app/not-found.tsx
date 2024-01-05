84db641cfd0c1241f59311fc0371081584b72675
SeryVathana <yooseryvathana@gmail.com>
2024-01-05 23:03:17 +0700

    Update not-found.tsx

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,11 +1,13 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import Link from 'next/link';
 import React from 'react';
 
-const NotFound = () => {
+const NotFound = async () => {
   return (
     <MaxWidthWrapper className='flex flex-col justify-center items-center min-h-[80vh]'>
       <h1 className='text-8xl tracking-wider text-red-500'>404</h1>
       <h6 className='text-xl'>Page Not Found</h6>
+      <Link href='/'>Return Home</Link>
     </MaxWidthWrapper>
   );
 };
