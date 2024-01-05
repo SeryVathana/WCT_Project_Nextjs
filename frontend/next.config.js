5bc1092f42e381eba7c64882b805e4f8f9e3ac79
SeryVathana <yooseryvathana@gmail.com>
2024-01-05 13:45:17 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'firebasestorage.googleapis.com', 'variety.com'],
   },
+  distDir: 'build',
 };
 
 module.exports = nextConfig;
