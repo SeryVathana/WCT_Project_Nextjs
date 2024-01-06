f6a21f3493af753d5f998cdb66cacf53b2d98c58
SeryVathana <yooseryvathana@gmail.com>
2024-01-06 08:22:21 +0700

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,12 @@ import userRouter from './routes/userRoute';
 import cors from 'cors';
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://auction-site-wct.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
